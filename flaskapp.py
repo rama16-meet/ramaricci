148f09a1074a62399c47430f4de685faeaa7a872
sela16-meet <sela16@meet.mit.edu>
2015-11-05 19:56:55 +0200

    not done

--- flaskapp.py
+++ flaskapp.py
@@ -3,13 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("rama.html")
+	return render_template("homerama.html")
 
-@app.route('/Hello/', methods=['POST'])
-@app.route('/Hello/<name>')
-def Hello():
-	a = request.form['name']
-	return render_template('rama3.html', name=a)
+@app.route('/aboutrama')
+def about():
+	return render_template("aboutrama.html")
+
+@app.route('/contactrama')
+def contact():
+	return render_template("contactrama.html")
+
+
 
 if __name__ == "__main__": 
 	app.run()
